9ad046ee5788d091ff9780ffa5c0d6924e01d4f7
kadir_tumturk <kdrtumturk98@gmail.com>
2026-05-09 21:57:45 +0300

    hata düzeltildi

--- js/task-1.js
+++ js/task-1.js
@@ -6,13 +6,8 @@ console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 cred
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
 
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-    let total = quantity * pricePerDroid;
-    if (customerCredits >= total) {
-        return `You ordered ${quantity} droids worth ${total} credits!`;
-    } else {
-        return `Insufficient funds!`;
-    }
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
